8ac5953f0d0c9df56527324ef078e8e8f08c34da
antoine <a.dupuy.pro@gmail.com>
2015-07-29 15:35:21 +0200

    Update random quarter

--- controllers/QuarterController.class.php
+++ controllers/QuarterController.class.php
@@ -34,16 +34,6 @@ class QuarterController extends BaseController {
 			'quarter' => $quarter,
 		);
 
-		$quarter_param = $this->getParam(0, '');
-
-		$quarter_id = 0;
-		$quarter_name = '';
-		if (!empty($quarter_param)) {
-			list($quarter_id, $quarter_name) = explode('-', $quarter_param);
-		}
-
-		echo $quarter_id;
-
 		//$quarter = Quarter::get($quarter_id);
 
 		$this->render('quarter', $vars);
